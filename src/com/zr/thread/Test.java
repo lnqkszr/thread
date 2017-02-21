b130edefbc1e13fa01d947c16716b9bf0f9c6778
zhurui <422522036@qq.com>
2017-02-21 11:40:39 +0800

    test

---
+++ src/com/zr/thread/Test.java
@@ -0,0 +1,16 @@
+package com.zr.thread;
+/**   
+* @Title: Test.java 
+* @Package thread 
+* @Description: 测试 
+* @author zhurui
+* @date 2017年2月21日 上午11:30:26 
+* @version V1.0   
+*/
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("hello ");
+	}
+
+}
